1a10e09ecfc060c6928f29c1103cb8b33a66e1c6
Lisa Knoff <lisa.katharina.knoff@gmail.com>
2026-05-28 11:28:28 +0200

    Make it possible to add data about new supermarkets, including comments and exception handling

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ A reporting tool for tracking carrot quality - or anything really ;)
 
 I did not use AI in this project. That does not mean that this project is of higher quality than my other projects (probably quite the opposite), but I believe in also making sure one can still code a little without AI. This is me making sure of that.
 
-So far, the project has no real functionality - it just fetches data from a csv file or returns an empty data frame if the file does not exist. Also, there are no tests in this project yet.
+So far, the project fetches data from a csv file or returns an empty data frame if the file does not exist. There is also a function (addSupermarket()) to add and save new data to the data frame.
+However, there are no tests in this project yet.
 
 Dependencies:
 pandas (imported as pd)
--- src/addSupermarket.py
+++ src/addSupermarket.py
@@ -1,5 +1,44 @@
 from getData import getData
 
 def addSupermarket():
+    # Get current data from supermarkets.csv
     data = getData()
+
+    # Get column names as list
+    columns = data.columns.values.tolist()
+    
+    # Create empty list for new supermarket
+    new_supermarket = list()
+
+    # Iterate over columns and request user input for each one
+    for column in columns:
+        print("Enter value for " + column)
+        value = input()
+        new_supermarket.append(value)
+    
+    print("You entered the following: ")
+    print(new_supermarket)
+    
+    print("Save to csv? y = yes, n = no")
+    answer = input()
+
+    # If user wants to save the dataframe, save it, else don't
+    if answer == "y":
+        # If there is data to be saved, save it. This works because you cannot save an empty 
+        # data frame, so to_csv raises ValueError then
+        try:
+            # Append new data to dataframe
+            data.loc[len(data)] = new_supermarket
+
+            # Save to csv
+            data.to_csv("../data/supermarkets.csv", index=False)
+            print("Saved!")
+        except ValueError:
+            if columns == []:
+                print("Could not save dataframe as it does not contain any data - consider adding columns first")
+    elif answer == "n":
+        print("Not saved!")
+    else:
+        print("Invalid entry")
+
     return 0
